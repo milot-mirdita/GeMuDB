07124deab3aa410b238060d74f673d8e93e661dd
Martin Steinegger <steinegger_martin@web.de>
2012-04-10 02:54:23 +0200

    service

--- src/org/rostlab/snapdb/service/model/MapElement.java
+++ src/org/rostlab/snapdb/service/model/MapElement.java
@@ -1,17 +1,19 @@
 package org.rostlab.snapdb.service.model;
 
 import javax.xml.bind.annotation.XmlElement;
+
 public class MapElement {
-    @XmlElement
-    public String key;
-    @XmlElement
-    public String value;
 
-    private MapElement() {
-    }
+	public MapElement() {
+	}
+	public MapElement(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
 
-    public MapElement(String key, String value) {
-        this.key = key;
-        this.value = value;
-    }
+	@XmlElement
+	public String key;
+
+	@XmlElement
+	public String value;
 }
