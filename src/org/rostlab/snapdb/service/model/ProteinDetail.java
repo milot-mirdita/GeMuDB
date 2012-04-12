60494e01a4321a014d1b13f0bd5f0c81b28427e5
Martin Steinegger <steinegger_martin@web.de>
2012-04-12 18:45:09 +0200

    proteindetail service

--- src/org/rostlab/snapdb/service/model/ProteinDetail.java
+++ src/org/rostlab/snapdb/service/model/ProteinDetail.java
@@ -1,38 +1,49 @@
 package org.rostlab.snapdb.service.model;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class ProteinDetail {
 	private String organismName;
 	private String definition;
 	private String officialGenFullName;
 	private String officialGenSymbol;
 	private String genLocation;
+	private String source;
 
+	@XmlElement(name = "organismName")
 	public String getOrganismName() {
 		return organismName;
 	}
 
+	@XmlElement(name = "definition")
 	public String getDefinition() {
 		return definition;
 	}
 
-	public void setOrganismName(String organismName) {
-		this.organismName = organismName;
-	}
-
-	public void setDefinition(String definition) {
-		this.definition = definition;
-	}
-
+	@XmlElement(name = "officialGenFullName")
 	public String getOfficialGenFullName() {
 		return officialGenFullName;
 	}
 
+	@XmlElement(name = "officialGenSymbol")
 	public String getOfficialGenSymbol() {
 		return officialGenSymbol;
 	}
 
+	@XmlElement(name = "genLocation")
 	public String getGenLocation() {
 		return genLocation;
+	}
+
+	@XmlElement(name = "source")
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
 	}
 
 	public void setOfficialGenFullName(String officialGenFullName) {
@@ -45,6 +56,14 @@ public class ProteinDetail {
 
 	public void setGenLocation(String genLocation) {
 		this.genLocation = genLocation;
+	}
+
+	public void setOrganismName(String organismName) {
+		this.organismName = organismName;
+	}
+
+	public void setDefinition(String definition) {
+		this.definition = definition;
 	}
 
 }
