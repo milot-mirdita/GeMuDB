17c05f7a1a2a0c01773f919324ad33bf8a0b2d77
Martin Steinegger <steinegger_martin@web.de>
2012-04-13 01:15:19 +0200

    import startup

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 libpath=
 for i in $(ls lib/*|grep ".jar"); do 
-	libpath=$( echo "$i;$libpath"); 
+	libpath=$( echo "$i:$libpath"); 
 done
 echo $libpath
-java -cp $(echo $libpath)build/jar/Snap2.jar org.rostlab.snapdb.Import $1
+java -cp "$(echo $libpath)build/jar/Snap2.jar" org.rostlab.snapdb.Import
