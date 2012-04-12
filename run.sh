b7390a8d4be2f85ce172c885ef33f51f2d9c65f9
Martin Steinegger <steinegger_martin@web.de>
2012-04-13 01:20:17 +0200

    bug^:wq

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ for i in $(ls lib/*|grep ".jar"); do
 	libpath=$( echo "$i:$libpath"); 
 done
 echo $libpath
-java -cp "$(echo $libpath)build/jar/Snap2.jar" org.rostlab.snapdb.Import
+java -cp "$(echo $libpath)build/jar/Snap2.jar" org.rostlab.snapdb.Import $1
