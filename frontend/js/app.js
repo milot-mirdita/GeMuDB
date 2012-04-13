be7a7f9f405208485cc05dce084995f0e908c7ae
Milot Mirdita <mail@brutos.org>
2012-04-13 16:05:54 +0200

    Work on urls

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -78,6 +78,26 @@ var initNumberMixin = (function(out) {
 	return out;
 });
 
+var initStringHelperMixin = (function(out) {
+	out.splitTypes = function(string) {
+		return string.toString().split(":");
+	};
+
+	out.buildTypeString = function(types) {
+		return types.join(":");
+	};
+
+	out.splitAlphabet = function(string) {
+		return string.toString().split("");
+	};
+
+	out.buildAlphabetString = function(chars) {
+		return chars.join("");
+	};
+	return out;
+});
+
+
 
 var Protein = function() {
 	this.refid = ko.observable("");
@@ -92,9 +112,12 @@ var Protein = function() {
 		var hidden = {};
 		initProteinMixin(hidden);
 		initNumberMixin(hidden);
+		initStringHelperMixin(hidden);
 		
 		// public
 		var self = this;
+
+		self.baseUrl = "http://localhost/api/resources/";
 
 		/**
 		 * @value protein id to search for
@@ -114,7 +137,7 @@ var Protein = function() {
 		/**
 		 * @arg type of details to show
 		 */
-		self.predictionType = ko.observableArray(["SNAP"]);
+		self.predictionTypes = ko.observableArray(["SNAP"]);
 		
 		/**
 		 * @arg length of detail graph
@@ -133,6 +156,8 @@ var Protein = function() {
 		/**
 		 * @arg 
 		 */
+		self.selectedIndex = ko.observable();
+		self.selectedMutation = ko.observable();
 		self.snpDetails = ko.observable();
 
 
@@ -140,17 +165,19 @@ var Protein = function() {
 			var depends2 = self.sequenceOffset();
 			self.listResult(null);
 			self.snpDetails(null);
+			self.selectedIndex(null);
+			self.selectedMutation(null);
 		});
 
 		ko.computed(function () {
 			if(self.proteinResult.refid()) {
 				var id =  self.proteinResult.refid();
-				var offset = (self.sequenceOffset() + 1);
+				var offset = ((self.sequenceOffset() - 0) + 1);
 				var length = self.lineLength;
 
-				$.when($.getJSON('http://localhost/api/resources/protein/mutations/' + id + '/' + offset + '/' + length))
+				$.when($.getJSON(self.baseUrl + 'protein/mutations/' + id + '/' + offset + '/' + length))
 				 .done(function (result) {
-				 	self.listResult(result.mutationsPos);
+			 		self.listResult(result.mutationsPos);
 				 })
 				 .fail(function (error) {
 				 	console.warn(error);
@@ -161,24 +188,7 @@ var Protein = function() {
 		self.searchProtein = function() {
 			location.hash = "!/search/" + self.protein;
 		};
-		
-		// self.switchType = function(type) {
-		// 	location.hash = "!/search/" + type + '/' + self.currentProtein;
-		// };
-		
-		self.toggleAmino = function(aa){
-			
-			
-		}
-
-		self.toggleType = function(type) {
-			var index = $.inArray(type, self.predictionType());
-			if(index == -1)
-				self.predictionType().push(type);
-			else
-				self.predictionType().splice(index, 1);
-		}
-		
+				
 		self.formatProteinResult = function(elements, data) {
 			var clickHandler = function (item) {
 				var index = self.sequenceOffset() + item.dataIndex + 1;
@@ -191,8 +201,8 @@ var Protein = function() {
 			
 			// hack: can't use a selector to query for .flot_container
 			// elements is a collection and not DOM
-			hidden.addGraph($('.flot_container'), data, self.predictionType(), false, clickHandler);
-			hidden.addGraph($('#flot_overview'), ko.toJS(self.proteinResult), self.predictionType(), true);
+			hidden.addGraph($('.flot_container'), data, self.predictionTypes(), false, clickHandler);
+			hidden.addGraph($('#flot_overview'), ko.toJS(self.proteinResult), self.predictionTypes(), true);
 		}
 
 		self.formatListResult = function(elements, data) {
@@ -202,6 +212,9 @@ var Protein = function() {
 		}
 
 		self.showDetailView = function(index, mutation) {
+			self.selectedIndex(index);
+			self.selectedMutation(mutation);
+
 			var snps = self.listResult();
 
 			for(var i in snps) {
@@ -218,6 +231,26 @@ var Protein = function() {
 			self.snpDetails(null);
 		}
 
+		self.alphabetInvalidator = ko.observable(0);
+		self.invalidateAlphabet = function () {
+			var value = self.alphabetInvalidator();
+			self.alphabetInvalidator(!value);
+		};
+
+		ko.computed(function () {
+			var depends = self.alphabetInvalidator();
+
+			var types = hidden.buildTypeString(self.predictionTypes());
+			var alphabet = $('#active_alphabet .active').text();
+			var offset = self.sequenceOffset() || 0;
+			var index = self.selectedIndex() || -1;
+			var mutation = self.selectedMutation() || -1;
+
+			location.hash = "!/show/" + self.protein + "/" + types + "/" + alphabet 
+								+ "/" + offset  + "/" + index + "/" + mutation;
+
+		}).extend({throttle:500});
+
 		Sammy(function() {
 			this.ajaxErrorHandler = function (error) {
 				console.log(error);
@@ -226,25 +259,36 @@ var Protein = function() {
 			this.get("#!/search/:protein", function(context) {
 				var protein = this.params["protein"];
 				
-				$.when($.post('http://localhost/api/resources/protein/search', 
+				$.when($.post(self.baseUrl + "protein/search", 
 						{q: protein}, undefined, "json"))
 					.done(function(searchResult) {
-						context.redirect('#!', 'show', searchResult.refid);
+						context.redirect("#!", "show", searchResult.refid, "SNAP:SIFT", 
+							"ACDEFGHIKLMNPQRSTVWY", "0", "-1", "-1");
 					})
 					.fail(this.ajaxErrorHandler);
 			});
 
-			this.get('#!/show/:protein', function(context) {
+			this.get("#!/show/:protein/:types/:alphabet/:offset/:index/:mutation", function(context) {
 				var protein = this.params["protein"];
-				if(self.currentProtein != protein) {
+				var alphabet = this.params["alphabet"];
+			
+				var chars = hidden.splitAlphabet(alphabet);
+				$("#active_alphabet .active").removeClass("active");
+				for(var c in chars) {
+					$("." + chars[c].toLowerCase(), "#active_alphabet").addClass("active");
+				}
+				self.invalidateAlphabet();
+
+				self.predictionTypes(hidden.splitTypes(this.params["types"]));
+				self.sequenceOffset(this.params["offset"] - 0);
+				self.showDetailView(this.params["index"], this.params["mutation"]);
+
+				if((self.currentProtein != protein) || (self.currentAlphabet != alphabet)) {
+					self.currentAlphabet = alphabet;
 					self.protein = self.currentProtein = protein;
 
-					var errorHandler = function (error) {
-						console.log(error);
-					};
-
-					$.when($.getJSON('http://localhost/api/resources/protein/prediction/' 
-									 + self.currentProtein))
+					$.when($.getJSON(self.baseUrl + "protein/prediction/" 
+									 + protein + "/" + alphabet))
 						.done(function (proteinResult) {
 							self.proteinResult.sequence(proteinResult.sequence);
 							self.proteinResult.refid(protein);
@@ -332,6 +376,45 @@ var Protein = function() {
 				var mutation = $(event.target).text();
 				var callback = valueAccessor();
 				callback(index, mutation);
+			});
+		}
+	};
+
+	ko.bindingHandlers.typeclick = {
+		init: function(element, valueAccessor, allBindingsAccessor, viewModel, context) {
+			$(element).click(function(event) {
+				var index = $.inArray(valueAccessor(), viewModel.predictionTypes());
+				if(index == -1)
+					viewModel.predictionTypes.push(valueAccessor());
+				else { 
+					if($("#active_types .active").size() > 1) {
+						viewModel.predictionTypes.splice(index, 1);
+					} else {
+						event.preventDefault();
+						event.stopPropagation();
+					} 
+				}
+			});
+		}
+	};
+
+	ko.bindingHandlers.alphabetclick = {
+		init: function(element, valueAccessor, allBindingsAccessor, viewModel, context) {
+			$(element).on("click", ".btn", function(event) {
+				var size = $("#active_alphabet .active").size();
+				if(!$(this).hasClass('active')) {
+					viewModel.invalidateAlphabet();
+					return;
+				}
+
+				if(size > 1) {
+					setTimeout(function() {
+						viewModel.invalidateAlphabet();
+					}, 20);
+				} else {
+					event.preventDefault();
+					event.stopPropagation();
+				} 
 			});
 		}
 	};
