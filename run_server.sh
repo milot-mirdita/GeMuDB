ca0181dbd01573d811a1c0405aa4af6db244273d
Martin Steinegger <steinegger_martin@web.de>
2012-04-16 01:34:11 +0200

    startup server

---
+++ run_server.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+libpath=
+for i in $(ls lib/*|grep ".jar"); do 
+	libpath=$( echo "$i:$libpath"); 
+done
+echo $libpath
+java -cp "$(echo $libpath)build/jar/Snap2.jar" org.rostlab.snapdb.Startup
