97a3ccc010ace2341aa1277b137463c5ff5eea1a
Milot Mirdita <milot@mirdita.de>
2014-01-10 21:32:40 +0100

    Fixed startup script

--- run_server.sh
+++ run_server.sh
@@ -3,4 +3,4 @@ libpath=
 for i in $(ls lib/*|grep ".jar"); do 
 	libpath=$( echo "$i:$libpath"); 
 done
-exec java -server -Xms32M -cp "$(echo $libpath)build/jar/Snap2.jar" org.rostlab.snapdb.Startup
+exec java -server -Xms32M -cp "$(echo $libpath)build/jar/Snap2.jar" org.rostlab.gemudb.Startup
