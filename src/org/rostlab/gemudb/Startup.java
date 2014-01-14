d8e88f234fdc5125d822cca08477cc36e2ce3079
Milot Mirdita <milot@mirdita.de>
2014-01-14 03:58:20 +0100

    NOW it shouldnt listen to 0.0.0.0 anymore

--- src/org/rostlab/gemudb/Startup.java
+++ src/org/rostlab/gemudb/Startup.java
@@ -1,21 +1,23 @@
 package org.rostlab.gemudb;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.URI;
-
-import javax.ws.rs.ext.RuntimeDelegate;
+import java.util.concurrent.Executors;
 
 import org.rostlab.gemudb.facade.ServiceFacade;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
-import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.jersey.api.container.ContainerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsServer;
 
 public class Startup {
 	/**
@@ -33,12 +35,21 @@ public class Startup {
 
 		HttpServer server;
 		try {
-			server = HttpServerFactory.create(URI.create("http://localhost:9004/"), rc);
+			final HttpHandler handler = ContainerFactory.createContainer(HttpHandler.class, rc);
+			final URI u = URI.create("http://localhost:9004/");
+			
+	        final String scheme = u.getScheme();
+	        final int port = (u.getPort() == -1) ? 80 : u.getPort();    
+	        server = (scheme.equalsIgnoreCase("http")) ? 
+	            HttpServer.create(new InetSocketAddress(InetAddress.getLocalHost(), port), 0) :
+	            HttpsServer.create(new InetSocketAddress(InetAddress.getLocalHost(), port), 0);
+
+	        server.setExecutor(Executors.newCachedThreadPool());
+	        server.createContext(u.getPath(), handler);        
+			
 			HttpContext httpcontext = server.createContext("/resources");
 			SnapDbApplication application = new SnapDbApplication(
 					(ServiceFacade) context.getBean("serviceFacade"));
-			HttpHandler handler = RuntimeDelegate.getInstance().createEndpoint(
-					application, HttpHandler.class);
 			httpcontext.setHandler(handler);
 			server.start();
 		} catch (IOException e) {
