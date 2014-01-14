089a45b2bf8fdd6efa036a0248d62ffb4df008b8
root <root@proteinsuite.com>
2014-01-14 13:57:28 +0400

    Use ipv4, dont listen to ipv6 socket

--- run_server.sh
+++ run_server.sh
@@ -4,4 +4,4 @@ libpath=
 for i in $(ls lib/*|grep ".jar"); do 
 	libpath=$( echo "$i:$libpath"); 
 done
-exec java -server -Xms32M -cp "$(echo $libpath)build/jar/Snap2.jar" org.rostlab.gemudb.Startup
+exec java -server -Xms32M -Djava.net.preferIPv4Stack=true -cp "$(echo $libpath)build/jar/Snap2.jar" org.rostlab.gemudb.Startup
